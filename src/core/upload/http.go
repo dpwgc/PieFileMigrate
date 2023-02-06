4aaa3a2e7fcdc42a9a136a5afbd07d55da62fa78
dpwgc <281148314@qq.com>
2023-02-06 18:37:32 +0800

    文件批量上传测试

--- src/core/upload/http.go
+++ src/core/upload/http.go
@@ -97,3 +97,88 @@ func (u *HTTPUploadHandler) UploadFile(fileName string, filePath string, modTime
 	// 只有当http状态码为200且返回数据为小写字符串'ok'时才会判定为上传成功
 	return nil
 }
+
+func (u *HTTPUploadHandler) UploadFiles(fileNames []string, filePaths []string, modTimes []time.Time) error {
+
+	bodyBuf := bytes.NewBufferString("")
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	boundary := bodyWriter.Boundary()
+
+	// 校验令牌
+	err := bodyWriter.WriteField("token", base.HttpConfig.Http.Token)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(fileNames); i++ {
+		// 上传文件
+		fw, err := bodyWriter.CreateFormFile("files", fileNames[i])
+		if err != nil {
+			return err
+		}
+
+		fh, err := os.Open(filePaths[i])
+		if err != nil {
+			fh.Close()
+			return err
+		}
+		_, err = io.Copy(fw, fh)
+		if err != nil {
+			fh.Close()
+			return err
+		}
+
+		// 文件命名
+		err = bodyWriter.WriteField("fileNames", fileNames[i])
+		if err != nil {
+			fh.Close()
+			return err
+		}
+
+		// 上传路径
+		err = bodyWriter.WriteField("filePaths", filePaths[i])
+		if err != nil {
+			fh.Close()
+			return err
+		}
+
+		// 文件最后修改时间
+		err = bodyWriter.WriteField("modTimes", fmt.Sprintf("%v", modTimes[i].UnixMilli()))
+		if err != nil {
+			fh.Close()
+			return err
+		}
+	}
+
+	requestReader := io.MultiReader(bodyBuf)
+
+	req, err := http.NewRequest("POST", base.HttpConfig.Http.TargetUrl, requestReader)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "multipart/form-data; boundary="+boundary)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New("status code != 200")
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		return err
+	}
+
+	if string(body) != "ok" {
+		return errors.New("response body != ok")
+	}
+
+	// 只有当http状态码为200且返回数据为小写字符串'ok'时才会判定为上传成功
+	return nil
+}
--- src/core/upload/interface.go
+++ src/core/upload/interface.go
@@ -5,4 +5,5 @@ import "time"
 type Handler interface {
 	// UploadFile 上传文件至其他服务器
 	UploadFile(fileName string, filePath string, modTime time.Time) error
+	UploadFiles(fileNames []string, filePaths []string, modTimes []time.Time) error
 }
